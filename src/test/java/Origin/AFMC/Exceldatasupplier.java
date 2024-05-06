80f6014873cf99cf035dcf81c08fcf7d075579a2
Prabhu <prabhu.m@acviss.com>
2024-05-06 18:20:40 +0530

    update

--- src/test/java/Origin/AFMC/Exceldatasupplier.java
+++ src/test/java/Origin/AFMC/Exceldatasupplier.java
@@ -1,4 +1,3 @@
-
 package Origin.AFMC;
 
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -17,7 +16,7 @@ public class Exceldatasupplier {
 
 	@DataProvider(name = "logindata")
 	public Object[][] getdata() throws IOException, ParseException {
-		File excelfile = new File("C:\\Users\\Acviss\\git\\repository8\\2AFMC\\Excel\\05-05-2024.xlsx");
+		File excelfile = new File("C:\\Users\\Acviss\\git\\repository8\\2AFMC\\Excel\\06-05-2024.xlsx");
 		FileInputStream fis = new FileInputStream(excelfile);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheet("Sheet1"); // Assuming the sheet name is "Sheet1"
